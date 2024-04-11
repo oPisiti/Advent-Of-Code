3b2fabc89130012b69c7f115a9f5b28444211f1f
oPisiti <laurohrfranca@gmail.com>
2024-04-11 17:11:39 -0300

    Should work :(

--- 2023/22/22.py
+++ 2023/22/22.py
@@ -17,6 +17,7 @@ class vec3d():
     def __sub__(self, other) -> None:
         return vec3d(self.x - other.x, self.y - other.y, self.z - other.z)
         
+
 class Brick():
     def __init__(self, a: vec3d, b: vec3d) -> None:
         self.a = a
@@ -31,7 +32,8 @@ class Brick():
             common_point = self.a if self.a.z == self.min_z else self.b
             self.z_plane_segment = [common_point, common_point]
 
-        self.supports_count = 0
+        self.support_of_indexes = []
+        self.supported_by_count = 0
 
     def apply_fall(self, d: int) -> None:
         if d < 0: raise ValueError("d must be a positive value")
@@ -43,7 +45,6 @@ class Brick():
         self.b.z = max(self.b.z - d, 1)
         self.min_z = min(self.a.z, self.b.z)
         self.max_z = max(self.a.z, self.b.z)
-
 
     def is_on_top(self, other) -> bool:
         # https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
@@ -108,36 +109,24 @@ def main():
     for i in range(len(bricks)):
         apply_physics(bricks, i)
 
+    # bricks.sort(key=lambda x: x.min_z)
+        
+
     # Determining how many bricks support a given brick
-    desintegratable = set()
+    desintegratable_count = 0
     for i in range(len(bricks)):
-        desintegratable.update(can_desintegrate(bricks, i))
-            # desintegratable += 1
-            # print(f"Can desintegrate brick {i}")
+        is_desin = True
 
-    # Adding top bricks to count
-    for i in range(len(bricks)):
-        if bricks[i].supports_count == 0:
-            desintegratable.add(i)
+        for index in bricks[i].support_of_indexes:
+            if bricks[index].supported_by_count <= 1:
+                is_desin = False
+                break
 
-    # print(f"{desintegratable = }")
-    print(f"{len(desintegratable) = }")
+        if is_desin: 
+            desintegratable_count += 1 
 
-
-def can_desintegrate(bricks: list[Brick], index: int) -> list[int]:
-    support_indexes = []
-    
-    for i, brick in enumerate(bricks):
-        if i == index: 
-            if len(support_indexes) > 1:
-                return support_indexes
-            else:
-                return []
-
-        # Checking for collision
-        if bricks[index].is_on_top(bricks[i]):
-            support_indexes.append(i)
-
+    print(f"{desintegratable_count = }")
+    pass
 
 
 def apply_physics(bricks: list[Brick], index: int) -> None:
@@ -145,10 +134,14 @@ def apply_physics(bricks: list[Brick], index: int) -> None:
         if bricks[index].min_z == 1: return   
         
         # Collision between current brick and one below
+        has_collided = False
         for i in range(index):
             if bricks[index].is_on_top(bricks[i]):
-                bricks[i].supports_count += 1
-                return
+                bricks[i].support_of_indexes.append(index)
+                bricks[index].supported_by_count += 1
+                has_collided = True
+        
+        if has_collided: return
 
         bricks[index].apply_fall(1) 
         
